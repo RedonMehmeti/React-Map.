2f4bc67576214fa83c56d7d0df72e85167d16529
RedonMehmeti <123652604+RedonMehmeti@users.noreply.github.com>
2025-10-02 18:51:03 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Qytetet from './qytetet';
+
+function App() {
+
+
+  return (
+    <>
+      <Qytetet/>
+      
+    </>
+  )
+}
+
+export default App
